e9034e1e0d949e7b95fd28d69f73a8dcac002b70
Klughertz Jonathan <klugjo@users.noreply.github.com>
2022-08-25 22:12:19 +0200

    chore: feedback

--- utils/strategies.py
+++ utils/strategies.py
@@ -96,20 +96,16 @@ BASE_STRATEGIES = [
     }
 ]
 
+STRATEGIES = []
 
-def get_strategies():
-    strategies = []
+for strategy in BASE_STRATEGIES:
+    legs = strategy["legs"]
+    is_structure_atm = any(leg["optionType"] == "call" and leg["price"] == "ATM" for leg in legs) and any(
+        leg["optionType"] == "put" and leg["price"] == "ATM" for leg in legs)
 
-    for strategy in BASE_STRATEGIES:
-        legs = strategy["legs"]
-        is_structure_atm = any(leg["optionType"] == "call" and leg["price"] == "ATM" for leg in legs) and any(
-            leg["optionType"] == "put" and leg["price"] == "ATM" for leg in legs)
+    strategy["fingerprint"] = "".join([get_leg_fingerprint(
+        leg["ratio"], leg["optionType"], is_structure_atm and leg["price"] == "ATM") for leg in legs])
+    strategy["inverted_fingerprint"] = "".join([get_leg_fingerprint(
+        leg["ratio"] * -1, leg["optionType"], is_structure_atm and leg["price"] == "ATM") for leg in legs])
 
-        strategy["fingerprint"] = "".join([get_leg_fingerprint(
-            leg["ratio"], leg["optionType"], is_structure_atm and leg["price"] == "ATM") for leg in legs])
-        strategy["inverted_fingerprint"] = "".join([get_leg_fingerprint(
-            leg["ratio"] * -1, leg["optionType"], is_structure_atm and leg["price"] == "ATM") for leg in legs])
-
-        strategies.append(strategy)
-
-    return strategies
+    STRATEGIES.append(strategy)
