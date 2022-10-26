997c34c5a006e4a716c9c48636fe298591e9238b
Tam Pham <tampham47@live.com>
2022-10-26 15:47:48 +0700

    feat: add call & put calendar strategies (#11)
    * feat: add call & put calendar strategies
    
    * chore: remove iron condor + iron fly

--- python/tests/test_strategy_detection.py
+++ python/tests/test_strategy_detection.py
@@ -6,14 +6,41 @@ def test_empty():
     assert s is None
 
 
+def test_call_cal():
+    s = get_strategy_name("BTC-20221028-22000C@-1/BTC-20221104-22500C@1")
+    assert s["longName"] == "Call Calendar Spread"
+    assert s["shortName"] == "CCal"
+
+    s = get_strategy_name("BTC-20221028-22000C@1/BTC-20221104-22500C@-1")
+    assert s["longName"] == "Call Calendar Spread"
+    assert s["shortName"] == "CCal"
+
+
+def test_put_cal():
+    s = get_strategy_name("ETH-20221028-1700P@-1/ETH-20221104-1750P@1")
+    assert s["longName"] == "Put Calendar Spread"
+    assert s["shortName"] == "PCal"
+
+    s = get_strategy_name("ETH-20221028-1700P@1/ETH-20221104-1750P@-1")
+    assert s["longName"] == "Put Calendar Spread"
+    assert s["shortName"] == "PCal"
+
 def test_call_spread():
     s = get_strategy_name("BTC-20220826-22000C@1/BTC-20220826-22500C@-1")
+    assert s["longName"] == "Call Spread"
+    assert s["shortName"] == "CSpd"
+
+    s = get_strategy_name("BTC-20220826-22000C@-1/BTC-20220826-22500C@1")
     assert s["longName"] == "Call Spread"
     assert s["shortName"] == "CSpd"
 
 
 def test_put_spread():
     s = get_strategy_name("ETH-20220826-1700P@-1/ETH-20220826-1750P@1")
+    assert s["longName"] == "Put Spread"
+    assert s["shortName"] == "PSpd"
+
+    s = get_strategy_name("ETH-20220826-1700P@1/ETH-20220826-1750P@-1")
     assert s["longName"] == "Put Spread"
     assert s["shortName"] == "PSpd"
 
@@ -36,25 +63,17 @@ def test_risk_reversal():
     assert s["shortName"] == "RRev"
 
 
-def test_iron_butterfly():
-    s = get_strategy_name(
-        "BTC-20220826-22000P@1/BTC-20220826-22000C@1/BTC-20220826-21500P@-1/BTC-20220826-22500C@-1")
-    assert s["longName"] == "Iron Butterfly"
-    assert s["shortName"] == "IrnFly"
-
-
 def test_call_condor():
     s = get_strategy_name(
         "BTC-20220826-23000C@-1/BTC-20220826-21000C@-1/BTC-20220826-21500C@1/BTC-20220826-22500C@1")
     assert s["longName"] == "Call Condor"
     assert s["shortName"] == "CCdr"
 
-
-def test_iron_condor():
+def test_put_condor():
     s = get_strategy_name(
-        "BTC-20220826-23000C@1/BTC-20220826-21000P@1/BTC-20220826-21500P@-1/BTC-20220826-22500C@-1")
-    assert s["longName"] == "Iron Condor"
-    assert s["shortName"] == "IrnCdr"
+        "BTC-20220826-23000P@-1/BTC-20220826-21000P@-1/BTC-20220826-21500P@1/BTC-20220826-22500P@1")
+    assert s["longName"] == "Put Condor"
+    assert s["shortName"] == "PCdr"
 
 
 def test_call_butterfly_spread():
--- python/utils/strategies.py
+++ python/utils/strategies.py
@@ -14,11 +14,27 @@ BASE_STRATEGIES = [
         ],
     },
     {
+        "longName": "Call Calendar Spread",
+        "shortName": "CCal",
+        "legs": [
+            {"optionType": "call", "price": "ATM", "ratio": -1},
+            {"optionType": "call", "price": "N_ATM", "ratio": 1},
+        ],
+    },
+    {
         "longName": "Put Spread",
         "shortName": "PSpd",
         "legs": [
-            {"optionType": "put", "price": "OTM", "ratio": -1},
-            {"optionType": "put", "price": "ATM", "ratio": 1},
+            {"optionType": "put", "price": "OTM", "ratio": 1},
+            {"optionType": "put", "price": "ATM", "ratio": -1},
+        ],
+    },
+    {
+        "longName": "Put Calendar Spread",
+        "shortName": "PCal",
+        "legs": [
+            {"optionType": "put", "price": "ATM", "ratio": -1},
+            {"optionType": "put", "price": "N_ATM", "ratio": 1},
         ],
     },
     {
@@ -46,16 +62,6 @@ BASE_STRATEGIES = [
         ],
     },
     {
-        "longName": "Iron Butterfly",
-        "shortName": "IrnFly",
-        "legs": [
-            {"optionType": "put", "price": "OTM", "ratio": 1},
-            {"optionType": "put", "price": "ATM", "ratio": -1},
-            {"optionType": "call", "price": "ATM", "ratio": -1},
-            {"optionType": "call", "price": "OTM", "ratio": 1},
-        ],
-    },
-    {
         "longName": "Call Condor",
         "shortName": "CCdr",
         "legs": [
@@ -66,13 +72,13 @@ BASE_STRATEGIES = [
         ],
     },
     {
-        "longName": "Iron Condor",
-        "shortName": "IrnCdr",
+        "longName": "Put Condor",
+        "shortName": "PCdr",
         "legs": [
             {"optionType": "put", "price": "X_OTM", "ratio": 1},
             {"optionType": "put", "price": "OTM", "ratio": -1},
-            {"optionType": "call", "price": "OTM", "ratio": -1},
-            {"optionType": "call", "price": "X_OTM", "ratio": 1},
+            {"optionType": "put", "price": "ATM", "ratio": -1},
+            {"optionType": "put", "price": "ITM", "ratio": 1},
         ],
     },
     {
@@ -101,10 +107,12 @@ for strategy in BASE_STRATEGIES:
     legs = strategy["legs"]
     is_structure_atm = any(leg["optionType"] == "call" and leg["price"] == "ATM" for leg in legs) and any(
         leg["optionType"] == "put" and leg["price"] == "ATM" for leg in legs)
+    is_multiple_expiry = any(leg["price"].find("N_") >= 0 for leg in legs)
 
-    strategy["fingerprint"] = "".join([get_leg_fingerprint(
+    strategy["fingerprint"] = ("N_" if is_multiple_expiry else "").join([get_leg_fingerprint(
         leg["ratio"], leg["optionType"], is_structure_atm and leg["price"] == "ATM") for leg in legs])
-    strategy["inverted_fingerprint"] = "".join([get_leg_fingerprint(
+
+    strategy["inverted_fingerprint"] = ("N_" if is_multiple_expiry else "").join([get_leg_fingerprint(
         leg["ratio"] * -1, leg["optionType"], is_structure_atm and leg["price"] == "ATM") for leg in legs])
 
     STRATEGIES.append(strategy)
